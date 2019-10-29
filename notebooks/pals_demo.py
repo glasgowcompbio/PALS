727e61c584db10e25a772f645d98a392c2b6719c
Joe Wandy <joewandy@gmail.com>
2019-10-29 12:12:09 +0000

    Updated notebook and demo script

--- notebooks/pals_demo.py
+++ notebooks/pals_demo.py
@@ -1,12 +1,15 @@
-import pandas as pd
 import os
+import pickle
 
-from pimp_tools import get_pimp_API_token_from_env, PIMP_HOST, get_ms1_intensities, get_ms1_peaks, get_formula_df
+import pandas as pd
+
 from feature_extraction import DataSource
 from pathway_analysis import PALS
+from pimp_tools import get_pimp_API_token_from_env, PIMP_HOST, get_ms1_intensities, get_formula_df, \
+    get_experimental_design
 
 token = get_pimp_API_token_from_env()
-analysis_id = 1321 # example beer analysis
+analysis_id = 1321  # example beer analysis
 
 int_df_filename = os.path.join(os.getcwd(), 'notebooks', 'test_data', 'int_df.p')
 try:
@@ -22,28 +25,17 @@ except FileNotFoundError:
     formula_df = get_formula_df(token, PIMP_HOST, analysis_id)
     formula_df.to_pickle('formula_df.p')
 
-experiment_design = {
-    'groups': {
-        'beer1': ['Beer_1_full1.mzXML', 'Beer_1_full2.mzXML', 'Beer_1_full3.mzXML'],
-        'beer2': ['Beer_2_full1.mzXML', 'Beer_2_full2.mzXML', 'Beer_2_full3.mzXML'],
-        'beer3': ['Beer_3_full1.mzXML', 'Beer_3_full2.mzXML', 'Beer_3_full3.mzXML'],
-        'beer4': ['Beer_4_full1.mzXML', 'Beer_4_full2.mzXML', 'Beer_4_full3.mzXML'],
-    },
-    'comparisons': [
-        {
-            'name': 'beer1/beer2',
-            'case': 'beer1',
-            'control': 'beer2'
-        },
-        {
-            'name': 'beer3/beer4',
-            'case': 'beer3',
-            'control': 'beer4'
-        },
-    ]
-}
+experimental_design_filename = os.path.join(os.getcwd(), 'test_data', 'experimental_design.p')
+try:
+    experimental_design_filename = os.path.join(os.getcwd(), 'test_data', 'experimental_design.p')
+    with open(experimental_design_filename, 'rb') as f:
+        experimental_design = pickle.load(f)
+except FileNotFoundError:
+    experimental_design = get_experimental_design(token, PIMP_HOST, analysis_id)
+    with open(experimental_design_filename, 'wb') as f:
+        pickle.dump(experimental_design, f)
 
-ds = DataSource(int_df, formula_df, experiment_design, database_name='kegg')
+ds = DataSource(int_df, formula_df, experimental_design, database_name='kegg')
 pals = PALS(ds, min_intensity=5000, num_resamples=10)
 activity_df = pals.get_plage_activity_df()
 plage_df = pals.set_up_resample_plage_p_df(activity_df)
