3b00152d668c2bb8ea69d62ca01f7a7ca9af43cc
Joe Wandy <joewandy@gmail.com>
2019-11-05 16:15:57 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'PALS',
+  packages = ['PALS'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Pathway-level Analysis of Metabolites Expression Data.',
+  author = 'Joe Wandy',
+  author_email = 'joe.wandy@glasgow.ac.uk',
+  url = 'https://github.com/glasgowcompbio/PALS', # use the URL to the github repo
+  keywords = ['pathway', 'metabolomics'], # arbitrary keywords
+  classifiers = [],
+)
