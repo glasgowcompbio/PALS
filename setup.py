0e023b9b927efb56db6eabfbc0dcd4c5755973ce
Joe Wandy <joewandy@gmail.com>
2019-11-06 14:16:32 +0000

    Increased minor version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PALS-pathway",
-    version="1.0.0",
+    version="1.0.1",
     author="Joe Wandy",
     author_email="joe.wandy@glasgow.ac.uk",
     description="Pathway-level Analysis of Metabolites Expression Data",
