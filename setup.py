91773b49be779609719edb3b039e6d68c7aca893
Joe Wandy <joewandy@gmail.com>
2019-11-06 17:16:25 +0000

    Correctly include data files in package

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="PALS-pathway",
-    version="1.0.2",
+    version="1.0.3",
     author="Joe Wandy",
     author_email="joe.wandy@glasgow.ac.uk",
     description="Pathway-level Analysis of Metabolites Expression Data",
     url="https://github.com/glasgowcompbio/PALS",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    packages=find_packages(),
+    package_data={
+        'pals': [
+            'data/*.json.zip',
+            'data/reactome/*/*/*.json.zip'
+        ],
+    }
 )
