13c0455ea5a72f0fc78f76f7ba81d7f300b64fb5
Joe Wandy <joewandy@gmail.com>
2019-11-25 14:16:15 +0000

    Updated example notebooks

--- notebooks/PALS_beer_analysis.py
+++ notebooks/PALS_beer_analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # %%
 
-from pals.pimp_tools import get_pimp_API_token_from_env, PIMP_HOST, get_ms1_intensities, get_ms1_peaks, get_formula_df, \
+from pals.pimp_tools import get_pimp_API_token_from_env, PIMP_HOST, get_ms1_intensities, get_ms1_peaks, get_annotation_df, \
     get_experimental_design
 from pals.feature_extraction import DataSource
 from pals.pathway_analysis import PALS
@@ -19,13 +19,12 @@ except FileNotFoundError:
     int_df = get_ms1_intensities(token, PIMP_HOST, analysis_id)
     int_df.to_pickle(int_df_filename)
 
-database_name = 'kegg'
-formula_df_filename = os.path.join(os.getcwd(), 'test_data', 'formula_df.p')
+annotation_df_filename = os.path.join(os.getcwd(), 'test_data', 'beer', 'annotation_df.p')
 try:
-    formula_df = pd.read_pickle(formula_df_filename)
+    annotation_df = pd.read_pickle(annotation_df_filename)
 except FileNotFoundError:
-    formula_df = get_formula_df(token, PIMP_HOST, analysis_id, database_name, polarity='positive')
-    formula_df.to_pickle(formula_df_filename)
+    annotation_df = get_annotation_df(token, PIMP_HOST, analysis_id)
+    annotation_df.to_pickle(annotation_df_filename)
 
 experimental_design_filename = os.path.join(os.getcwd(), 'test_data', 'experimental_design.p')
 try:
@@ -37,8 +36,8 @@ except FileNotFoundError:
     with open(experimental_design_filename, 'wb') as f:
         pickle.dump(experimental_design, f)
 
-ds = DataSource(int_df, formula_df, experimental_design, DATABASE_PIMP_KEGG)
-pals = PALS(ds, min_replace=5000)
+ds = DataSource(int_df, annotation_df, experimental_design, DATABASE_PIMP_KEGG)
+pals = PALS(ds, min_replace=5000, plage_weight=5, hg_weight=1)
 pathway_df = pals.get_pathway_df()
 output = os.path.join(os.getcwd(), 'test_data', 'pathway_df_pimp_kegg.csv')
 pathway_df.to_csv(output)
