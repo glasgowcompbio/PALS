a8e89ac113d71c00185d5713b3ebba0796d8c255
Joe Wandy <joewandy@gmail.com>
2020-03-26 23:46:34 +0000

    Improved diagram by submitting expression values to Reactome

--- pals/run_gui.py
+++ pals/run_gui.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+from urllib.parse import quote
 
+import numpy as np
 import requests
 import streamlit as st
 
@@ -35,8 +37,11 @@ def main():
         'datasets from metabolomics experiments. PALS is a Python package to perform the ranking of '
         'significantly-changing metabolite pathways in different experimental conditions through the decomposition of '
         'pathway activity levels calculated from peak intensities.')
-    st.write('To begin, please upload your intensity and annotation matrices from the sidebar. Select a case and'
-             'control group, the pathway analysis method as well as the database to use.')
+    st.write('To begin, please upload your intensity ([example](https://raw.githubusercontent.com/glasgowcompbio/PALS/'
+             'master/notebooks/test_data/beer/int_df.csv)) and annotation ([example](https://raw.githubusercontent.com/'
+             'glasgowcompbio/PALS/master/notebooks/test_data/beer/annotation_df.csv)) matrices from the sidebar. Next, '
+             'select a case and control group, the pathway analysis method as well as the database to use. PALS will '
+             'perform pathway analysis and display the results below.')
 
     intensity_csv = st.sidebar.file_uploader("Choose an intensity CSV file", type=['txt', 'csv'])
     annotation_csv = st.sidebar.file_uploader("Choose an annotation CSV file", type=['txt', 'csv'])
@@ -109,17 +114,29 @@ def main():
             reactome_query = st.sidebar.checkbox('Connect to a Reactome Neo4j database (Online Mode).', value=False)
 
         significant_column = '%s/%s p-value' % (case, control)
-        ds = DataSource(int_df, annotation_df, experimental_design, database_name,
-                        reactome_species=reactome_species,
-                        reactome_metabolic_pathway_only=reactome_metabolic_pathway_only,
-                        reactome_query=reactome_query, min_replace=min_replace)
+        ds = get_data_source(annotation_df, database_name, experimental_design, int_df, min_replace,
+                             reactome_metabolic_pathway_only, reactome_query, reactome_species)
 
         if len(ds.dataset_pathways) == 0:
             st.error('No matching pathways found for this data. Ensure that the database and species are correct.')
         else:
             df = pathway_analysis(significant_column, selected_method, ds)
             st.success('Pathway analysis successful!')
-            process_results(significant_column, df, use_reactome)
+
+            token = None
+            if use_reactome:
+                token = send_expression_data(ds, case, control, reactome_species)
+            process_results(significant_column, df, use_reactome, token)
+
+
+@st.cache
+def get_data_source(annotation_df, database_name, experimental_design, int_df, min_replace,
+                    reactome_metabolic_pathway_only, reactome_query, reactome_species):
+    ds = DataSource(int_df, annotation_df, experimental_design, database_name,
+                    reactome_species=reactome_species,
+                    reactome_metabolic_pathway_only=reactome_metabolic_pathway_only,
+                    reactome_query=reactome_query, min_replace=min_replace)
+    return ds
 
 
 @st.cache
@@ -145,25 +162,7 @@ def pathway_analysis(significant_column, selected_method, ds):
     return df
 
 
-@st.cache
-def send_to_reactome(stId):
-    # refer to https://reactome.org/dev/content-service
-    url = 'https://reactome.org/ContentService/data/query/%s' % stId
-    logger.debug('Reactome URL: ' + url)
-
-    # make a GET request to Reactome Content service
-    response = requests.get(url)
-    logger.debug('Response status code = %d' % response.status_code)
-
-    status_code = response.status_code
-    if status_code == 200:
-        json_response = json.loads(response.text)
-    else:
-        json_response = None
-    return status_code, json_response
-
-
-def process_results(significant_column, df, use_reactome):
+def process_results(significant_column, df, use_reactome, token):
     # reorder and rename columns
     df = df[['pw_name', significant_column, 'tot_ds_F', 'unq_pw_F', 'F_coverage']]
     df = df.rename(columns={
@@ -194,8 +193,9 @@ def process_results(significant_column, df, use_reactome):
 
     if use_reactome:
         # write header -- pathway info
-        st.header('Pathway Information')
-        st.write('The following shows additional information on the selected pathways.')
+        st.header('Pathway Browser')
+        st.write('To display additional information on significantly changing pathways, please select them in'
+                 ' the selection below.')
 
         choices = []
         for idx, row in df.iterrows():
@@ -210,14 +210,18 @@ def process_results(significant_column, df, use_reactome):
             stId = tokens[1].strip()
             stId = stId[:-1]  # remove last ')' character from stId
 
-            status_code, json_response = send_to_reactome(stId)
+            status_code, json_response = get_reactome_info(stId)
             if status_code == 200:
-                logger.debug(json_response)
+                # logger.debug(json_response)
 
                 # st.subheader(pw)
-                link_label = '%s (%s)' % (pw_name, stId)
-                link_url = 'https://reactome.org/content/detail/%s' % stId
-                st.write('### [%s](%s)' % (link_label, link_url))
+                label = '%s: %s' % (stId, pw_name)
+                info_url = 'https://reactome.org/content/detail/%s' % stId
+                header_markdown = '### %s [[info]](%s)' % (label, info_url)
+                if token is not None:
+                    viewer_url = 'https://reactome.org/PathwayBrowser/#/%s&DTAB=AN&ANALYSIS=%s' % (stId, token)
+                    header_markdown += ' [[viewer]](%s)' % viewer_url
+                st.write(header_markdown)
 
                 row = df.loc[stId]
                 pvalue = row[significant_column]
@@ -232,15 +236,112 @@ def process_results(significant_column, df, use_reactome):
                     summary = summation['text']
                     st.write(summary)
 
-                image_url = 'https://reactome.org/ContentService/exporter/diagram/%s.png?quality=8&' \
-                            'diagramProfile=standard&analysisProfile=strosobar' % stId
+                image_url = 'https://reactome.org/ContentService/exporter/diagram/%s.png?quality=8' \
+                            '&diagramProfile=standard&analysisProfile=strosobar' % stId
+                if token is not None:
+                    image_url += '&token=%s' % token
+                logger.debug('image_url = %s' % image_url)
                 st.image(image_url, use_column_width=True)
 
+                # print reactions
                 # for event in json_response['hasEvent']:
                 #     name = event['name'][0]
                 #     species = event['speciesName']
                 #     event_str = '- %s (%s)' % (name, species)
                 #     st.write(event_str)
+
+
+def send_expression_data(ds, case, control, species):
+    # send expression data to reactome for diagram exporter
+    int_df = ds.change_zero_peak_ints()
+    annot_df = ds.get_annotations()
+    design = ds.get_experimental_design()
+
+    case_cols = design['groups'][case]
+    control_cols = design['groups'][control]
+
+    df = annot_df.join(int_df).set_index('entity_id').sort_index()
+    case_df = np.log2(df[case_cols])
+    control_df = np.log2(df[control_cols])
+    lfcs = np.mean(case_df, axis=1) - np.mean(control_df, axis=1)
+
+    # if there are multiple values for each compound, take the largest fold-change (in either direction) to display
+    # TODO: we need a better way to do this
+    fold_changes = {}
+    for idx, lfc in lfcs.iteritems():
+        if idx not in fold_changes:
+            fold_changes[idx] = lfc
+        else:
+            if abs(lfc) > abs(fold_changes[idx]):
+                fold_changes[idx] = lfc
+
+    expression_df = pd.DataFrame.from_dict(fold_changes.items()).set_index(0)
+    expression_df = expression_df.rename(columns={1: 'log_FC'})
+    expression_df.index.name = '#id'
+
+    expression_data = expression_df.to_csv(sep='\t', header=True, index_label='#id', float_format='%.15f')
+    encoded_species = quote(species)
+
+    status_code, json_response = send_reactome_expression_data(expression_data, encoded_species)
+    if status_code == 200:
+        pathways_df, reactome_url, reactome_token = parse_reactome_json(json_response)
+        logger.debug('reactome_url = %s' % reactome_url)
+        logger.debug('reactome_token = %s' % reactome_token)
+        return reactome_token
+    else:
+        return None
+
+
+@st.cache
+def get_reactome_info(stId):
+    # refer to https://reactome.org/dev/content-service
+    url = 'https://reactome.org/ContentService/data/query/%s' % stId
+    logger.debug('Reactome URL: ' + url)
+
+    # make a GET request to Reactome Content service
+    response = requests.get(url)
+    logger.debug('Response status code = %d' % response.status_code)
+
+    status_code = response.status_code
+    if status_code == 200:
+        json_response = json.loads(response.text)
+    else:
+        json_response = None
+    return status_code, json_response
+
+
+@st.cache
+def send_reactome_expression_data(data, encoded_species):
+    # refer to https://reactome.org/AnalysisService/#/identifiers/getPostTextUsingPOST
+    url = 'https://reactome.org/AnalysisService/identifiers/?interactors=false&species=' + encoded_species + \
+          '&sortBy=ENTITIES_PVALUE&order=ASC&resource=TOTAL&pValue=1&includeDisease=true'
+    logger.debug('Reactome URL: ' + url)
+
+    # make a POSt request to Reactome Analysis service
+    response = requests.post(url, headers={'Content-Type': 'text/plain'}, data=data.encode('utf-8'))
+    logger.debug('Response status code = %d' % response.status_code)
+
+    status_code = response.status_code
+    if status_code == 200:
+        json_response = json.loads(response.text)
+    else:
+        json_response = None
+    return status_code, json_response
+
+
+def parse_reactome_json(json_response):
+    # see https://reactome.org/userguide/analysis for results explanation
+    token = json_response['summary']['token']
+    pathways = json_response['pathways']
+
+    reactome_url = 'https://reactome.org/PathwayBrowser/#DTAB=AN&ANALYSIS=' + token
+    logger.debug('Pathway analysis token: ' + token)
+    logger.debug('Pathway analysis URL: ' + reactome_url)
+
+    # https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
+    pathways_df = pd.json_normalize(pathways, sep='_')
+    return pathways_df, reactome_url, token
+
 
 max_width()
 main()
