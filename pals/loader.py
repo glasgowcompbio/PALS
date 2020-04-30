6bfa8f8af4bb7f8bfa087599d8b7c90a071ea690
Joe Wandy <joewandy@gmail.com>
2020-04-30 12:30:36 +0100

    Integrated GNPS Loader

--- pals/loader.py
+++ pals/loader.py
@@ -1,6 +1,11 @@
 import os
 
 from loguru import logger
+import zipfile
+import pandas as pd
+import requests, zipfile
+from io import StringIO, BytesIO
+from tqdm import tqdm
 
 from .common import DATABASE_PIMP_KEGG, load_json, DATA_DIR
 from .reactome import get_pathway_dict, get_compound_mapping_dict, load_entity_dict, get_protein_entity_dict, \
@@ -8,11 +13,12 @@ from .reactome import get_pathway_dict, get_compound_mapping_dict, load_entity_d
 
 
 class Database(object):
-    def __init__(self, database_name, pathway_dict, entity_dict, mapping_dict):
+    def __init__(self, database_name, pathway_dict, entity_dict, mapping_dict, extra_data=None):
         self.database_name = database_name
         self.pathway_dict = pathway_dict
         self.entity_dict = entity_dict
         self.mapping_dict = mapping_dict
+        self.extra_data = extra_data
 
     def __repr__(self):
         return self.database_name
@@ -115,3 +121,153 @@ class EnsemblLoader(Loader):
         }
         database = Database(self.database_name, data['pathway_dict'], data['entity_dict'], data['mapping_dict'])
         return database
+
+
+class GNPSLoader(Loader):
+    def __init__(self, database_name, gnps_url, metadata_df, comparisons):
+        self.database_name = database_name
+        self.gnps_url = gnps_url
+        self.metadata_df = metadata_df
+        self.comparisons = comparisons
+        self.int_df = None
+        self.annotation_df = None
+
+    def load_data(self):
+        # load clustering and quantification info from the zip file
+        clustering_df, quantification_df = self._download_gnps(self.gnps_url)
+        assert clustering_df is not None and quantification_df is not None
+
+        # drop all the singleton components
+        clustering_df = clustering_df[clustering_df['componentindex'] != -1]
+
+        # keep only columns containing 'Peak area', and remove 'Peak area' from column names
+        measurement_df = quantification_df.filter(regex='Peak area')
+        measurement_df.columns = measurement_df.columns.str.rstrip('Peak area')
+        measurement_df.index.rename('peak_id', inplace=True)
+        measurement_df.index = measurement_df.index.astype('str')
+
+        # assume metadata_df has two columns: 'sample' and 'group'
+        # remove rows with sample id that can't be found in the columns of int_df
+        metadata_df = self.metadata_df[self.metadata_df['sample'].isin(measurement_df.columns.values)]
+
+        # keep only columns in int_df that have group information
+        measurement_df = measurement_df[metadata_df['sample']]
+
+        # create annotation dataframe
+        annotation_df = pd.DataFrame(index=clustering_df.index)
+        annotation_df.index.rename('peak_id', inplace=True)
+        annotation_df['entity_id'] = clustering_df.index
+        annotation_df['entity_id'] = annotation_df['entity_id'].astype(str)
+        annotation_df.index = annotation_df.index.astype('str')
+
+        # create experimental design dictionary
+        groups = {}
+        for k, v in metadata_df.groupby('group'):
+            groups[k] = v['sample'].values.tolist()
+
+        experimental_design = {
+            'comparisons': self.comparisons,
+            'groups': groups
+        }
+
+        # combine all above into the extra_data dictionary for a Database
+        extra_data = {
+            'measurement_df': measurement_df,
+            'annotation_df': annotation_df,
+            'experimental_design': experimental_design
+        }
+
+        # generate a database for GNPS
+        database = self._to_database(clustering_df, extra_data)
+        return database
+
+    def _to_database(self, clustering_df, extra_data):
+        """
+        Creates a user-defined database for GNPS
+        :param clustering_df: a dataframe of clustering information
+        :param extra_data: additional information to include in the database
+        :return: a Database object for GNPS
+        """
+
+        # First create 'pathway' dictionary. In this case, 'pathway' is a GNPS molecular family / component
+        pathway_dict = {}
+        for comp in clustering_df['componentindex'].values:
+            key = str(comp)
+            pathway_dict[key] = {'display_name': 'Molecular Family #%d' % comp}
+
+        # Create entity dictionary. In this case, an 'entity' is a MS1 peak (GNPS consensus cluster)
+        # turn df to dictionary, with cluster index as the key
+        clustering_df.index = clustering_df.index.astype('str')
+        entity_dict = clustering_df.to_dict(orient='index')
+        for peak_id in entity_dict:
+            entity_dict[peak_id]['unique_id'] = peak_id
+            entity_dict[peak_id]['display_name'] = entity_dict[peak_id]['parent mass']
+
+        # Create mapping dictionary that maps entities to pathways
+        mapping_dict = {}
+        for peak_id in entity_dict:
+            component_index = str(entity_dict[peak_id]['componentindex'])
+            mapping_dict[peak_id] = [component_index]
+
+        # put everything together in a Database object
+        database = Database(self.database_name, pathway_dict, entity_dict, mapping_dict, extra_data=extra_data)
+        return database
+
+    def _download_gnps(self, gnps_url):
+        """
+        Downloads the zipped cytoscape data from GNPS and extract clustering and quantification dataframes from it.
+        :param gnps_url: the url to the GNPS experiment, e.g.
+        https://gnps.ucsd.edu/ProteoSAFe/status.jsp?task=0a8432b5891a48d7ad8459ba4a89969f
+        :return: clustering and quantification dataframes from that GNPS result
+        """
+        # extract task id from the url
+        tokens = gnps_url.split('task=')
+        task = tokens[1]
+        logger.info('Found GNPS task %s' % task)
+
+        # send a post request to GNPS
+        data = {
+            'task': task,
+            'view': 'download_cytoscape_data'
+        }
+        api_endpoint = 'https://gnps.ucsd.edu/ProteoSAFe/DownloadResult'
+        r = requests.post(url=api_endpoint, data=data, stream=True)
+
+        # extract clustering and quantification tables
+        # https://stackoverflow.com/questions/37573483/progress-bar-while-download-file-over-http-with-requests
+        clustering_df = None
+        quantification_df = None
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 1024
+
+        t = tqdm(total=total_size, unit='iB', unit_scale=True)
+        with BytesIO() as f:
+            for data in r.iter_content(block_size):
+                t.update(len(data))
+                f.write(data)
+            clustering_df, quantification_df = self._parse_gnps(f)
+        t.close()
+
+        return clustering_df, quantification_df
+
+    def _parse_gnps(self, input_stream):
+        """
+        Parses a zipped GNPS input stream, and extract clustering and quantification tables
+        :param input_stream: a zipped input of GNPS results
+        :return: clustering and quantification tables from the zip file
+        """
+        clustering_df = None
+        quantification_df = None
+        with zipfile.ZipFile(input_stream) as z:
+            for filename in z.namelist():
+                # read clustering information
+                if filename.startswith('clusterinfo_summary'):
+                    logger.debug('Found cluster info: %s' % filename)
+                    clustering_df = pd.read_csv(z.open(filename), sep='\t', index_col='cluster index')
+
+                # read quantification information
+                if filename.startswith('quantification_table'):
+                    logger.debug('Found quantification table: %s' % filename)
+                    quantification_df = pd.read_csv(z.open(filename), sep=',').set_index('row ID')
+
+        return clustering_df, quantification_df
