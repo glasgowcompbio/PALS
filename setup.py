ac01a10a68e111ecfed96f105878b2cf78af2f95
Joe Wandy <joewandy@gmail.com>
2020-05-22 22:07:12 +0100

    Updated Pipfile and setup.py to include correct dependencies and entry points (issue #13)

--- setup.py
+++ setup.py
@@ -5,17 +5,20 @@ with open("README.md", "r") as fh:
 
 setup(
     name="PALS-pathway",
-    version="1.3.1",
+    version="1.4.0",
     author="Joe Wandy",
     author_email="joe.wandy@glasgow.ac.uk",
-    description="Pathway-level Analysis of Metabolites Expression Data",
+    description="A Python tool to rank significantly-changing metabolite sets",
     url="https://github.com/glasgowcompbio/PALS",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    scripts=['pals/run.py'],
+    entry_points={
+        'console_scripts': ['run_pals=pals.run_pals:main'],
+    },
+    scripts=['pals/run_pals.py'],
     python_requires='>=3.6',
     packages=find_packages(),
     package_data={
@@ -23,5 +26,7 @@ setup(
             'data/*.json.zip',
             'data/reactome/*/*/*.json.zip'
         ],
-    }
+    },
+    install_requires=['numpy', 'scipy', 'pandas', 'seaborn', 'scikit-learn', 'matplotlib', 'statsmodels',
+                      'loguru', 'requests', 'neo4j-driver', 'gseapy==0.9.16', 'streamlit', 'tqdm', 'pillow'],
 )
